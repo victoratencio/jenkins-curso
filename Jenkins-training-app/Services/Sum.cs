c8706f4e323f9c76413abc56f56467c39eb11342
Victor Atencio <vatencio@itx.com>
2024-11-26 16:23:19 -0300

    adding unit test project

---
+++ Jenkins-training-app/Services/Sum.cs
@@ -0,0 +1,15 @@
+﻿namespace Jenkins_training_app.Services
+{
+	public class Sum
+	{
+		public int SumValues(int v1, int v2)
+		{
+			return v1 + v2;
+		}
+
+		public int RestValues(int v1, int v2)
+		{
+			return v1 - v2;
+		}
+	}
+}
---
+++ XUnitTest/UnitTest1.cs
@@ -0,0 +1,25 @@
+namespace XUnitTest
+{
+	public class XUnitTest
+	{
+		[Fact]
+		public void SumValuesTest()
+		{
+			Jenkins_training_app.Services.Sum Sum = new  Jenkins_training_app.Services.Sum();
+
+			var result = Sum.SumValues(5, 2);
+
+			Assert.Equal(7, result);
+		}
+
+		[Fact]
+		public void RestValuesTest()
+		{
+			Jenkins_training_app.Services.Sum Rest = new Jenkins_training_app.Services.Sum();
+
+			var result = Rest.RestValues(5, 2);
+
+			Assert.Equal(3, result);
+		}
+	}
+}
